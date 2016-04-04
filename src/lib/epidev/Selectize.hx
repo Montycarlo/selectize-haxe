71661a52078c3e440bf40c1b09be5106e02c247b
Montycarlo <joshua.brown@au.ey.com>
2016-04-04 12:12:52 +1000

    minor comments

--- src/lib/epidev/Selectize.hx
+++ src/lib/epidev/Selectize.hx
@@ -7,11 +7,21 @@ using Lambda;
 
 
 interface ISelectizeOption{
+	/*
+	Interface for converting an object to a selectize option.
+	Classes which implement this should return a newly-constructed SelectizeOption,
+	 	public function selectize() return new SelectizeOption(this.name, this.value);
+	which will represent the object in the Selectize box.
+	*/
 	function selectize():SelectizeOption;
 }
 
 @final
 class SelectizeOption implements ISelectizeOption{
+	/*
+	A object which represents an option in a Selectize box.
+	Just contains a name and value field, which will be substituted in the HTML <option> tag.
+	*/
 	public var name:String;
 	public var val:String;
 	public function new(n:String, v:String){
@@ -32,6 +42,15 @@ typedef SelectizeState = {};
 
 @final
 class Selectize extends ReactComponentOfPropsAndState<SelectizeProps, SelectizeState>{
+	/*
+	Main class for this package. Responsible for interfacing with React and the actual selectize.js calls.
+	Setting the properties is done via a parent object's JSX render() method.
+
+			jsx('<Selectize ops=${getOps()}')
+
+	There's a number of configurable properties, many of which are optional. Check the SelectizeProps typedef,
+	or the samples directory to check for appropriate usage or copypasta.	
+	*/
 
 	override public function render(){
 		var label = props.label != null ? jsx('<label className="control-label">${props.label}</label>') : null;
@@ -57,6 +76,8 @@ class Selectize extends ReactComponentOfPropsAndState<SelectizeProps, SelectizeS
 	}
 
 	override public function componentDidMount(){
+		/* TODO: Fix this up, untyped should be eliminated as the JQuery lib should be present and
+		 the selectize extern should be created. #sel should also be patched.*/
 		untyped new JQuery("#sel").selectize({
 			create:props.create
 		});
